e0390680207ae1cf73eada35ba6f56640cd06e2d
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2023-08-26 15:44:39 +0000

    upgrade torch

--- dev/python_setup.sh
+++ dev/python_setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh
+set -uex
 
 project_path=${0:a:h:h}
 
 mkdir -p $project_path/local
 
-python3 -m virtualenv $project_path/local/venv
+python3 -m venv $project_path/local/venv
 
 $project_path/local/venv/bin/pip install -r $project_path/dev/requirements.txt
